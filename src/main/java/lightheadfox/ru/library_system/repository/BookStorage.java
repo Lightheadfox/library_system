0ab43c8d759a0b2a49a7d781c62bd0697f8f25d1
Lightheadfox <lightheadfox@yandex.ru>
2024-09-23 16:53:26 +0300

    Первое приближение по имплементации поиска в приложении.

--- src/main/java/lightheadfox/ru/library_system/repository/BookStorage.java
+++ src/main/java/lightheadfox/ru/library_system/repository/BookStorage.java
@@ -3,9 +3,30 @@ package lightheadfox.ru.library_system.repository;
 import lightheadfox.ru.library_system.domain.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface BookStorage extends JpaRepository<Book, Long> {
 
 
     void deleteBookById(Long id);
+
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorContainingIgnoreCaseAndTitleContainingIgnoreCase(String author, String title);
+
+    List<Book> findByDescriptionContainsIgnoreCase(String description);
+
+    @Query(value = "SELECT * FROM book WHERE " +
+            "SIMILARITY(author, :searchTerm) > 0.3 OR " +
+            "SIMILARITY(title, :searchTerm) > 0.3 OR " +
+            "SIMILARITY(description, :searchTerm) > 0.3 " +
+            "ORDER BY GREATEST ((SIMILARITY(author, :searchTerm), SIMILARITY(title, :searchTerm), SIMILARITY(description, :searchTerm))) DESC",
+            nativeQuery = true)
+    List<Book> fuzzySearchBooks(@Param("searchTerm") String searchTerm);
 }
--- src/main/java/lightheadfox/ru/library_system/service/BookInterface.java
+++ src/main/java/lightheadfox/ru/library_system/service/BookInterface.java
@@ -3,6 +3,8 @@ package lightheadfox.ru.library_system.service;
 import lightheadfox.ru.library_system.domain.Book;
 import lightheadfox.ru.library_system.domain.BookDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -19,5 +21,12 @@ public interface BookInterface {
 
     void updateBook(Long id, BookDTO bookDTO);
 
+    //List<Book> fuzzySearch(String searchTerm);
+
+    @Query(value = "SELECT * FROM book WHERE " +
+            "SIMILARITY(author, :searchTerm) > 0.3 " +
+            "ORDER BY GREATEST (SIMILARITY(author, :searchTerm)) DESC",
+            nativeQuery = true)
+    List<Book> fuzzySearchBooks(@Param("searchTerm") String searchTerm);
 
 }
