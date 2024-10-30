8ccc616039d12eaacca61cf65fbfc602e17781d4
Lightheadfox <lightheadfox@yandex.ru>
2024-10-30 12:17:35 +0300

    Back up

--- src/main/java/lightheadfox/ru/library_system/config/SecurityConfig.java
+++ src/main/java/lightheadfox/ru/library_system/config/SecurityConfig.java
@@ -1,20 +1,12 @@
 package lightheadfox.ru.library_system.config;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
